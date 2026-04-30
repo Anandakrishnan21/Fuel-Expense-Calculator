821a8489c582260b76a6a46f955520bf64b22b61
Anandakrishnan21 <anandakrishnandinej02@gmail.com>
2026-04-30 19:51:03 +0530

    Initial commit

---
+++ src/components/icons/MinusIcon.tsx
@@ -0,0 +1,20 @@
+function MinusIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-minus-icon lucide-minus"
+    >
+      <path d="M5 12h14" />
+    </svg>
+  )
+}
+
+export default MinusIcon
